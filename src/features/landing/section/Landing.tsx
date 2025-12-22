3b4d289d76454450cb820f4f85514afbd7d8fd8a
Gibum <gudeng0611@cau.ac.kr>
2025-12-22 18:25:02 +0900

    feat: add initial landing page components
     - 사전 작성한 hero section과 이미지를 구성하여 landing page 뼈대 작성
       - 이미지는 반응형으로 mobile, tablet, desktop에서 다른 이미지를
         사용하도록 설정

---
+++ src/features/landing/section/Landing.tsx
@@ -0,0 +1,25 @@
+import LandingHero from '../components/LandingHeroOrg/LandingHeorOrgn';
+
+const Landing = () => (
+  <div className="flex flex-col items-center w-full overflow-hidden bg-white">
+    <LandingHero />
+
+    {/* 반응형 이미지 (mobile, tablet, desktop에 따라 다른 이미지 사용) */}
+    <div className="w-full flex justify-center px-4 pb-20">
+      <picture>
+        <source media="(min-width: 1024px)" srcSet="/images/landing-desktop.svg" />
+        <source media="(min-width: 768px)" srcSet="/images/landing-tablet.svg" />
+        <img
+          src="/images/landing-mobile.svg"
+          alt="Snack App Interface"
+          className="
+              w-full 
+              max-w-[375px] tablet:max-w-[768px] desktop:max-w-[1240px]
+              h-auto object-cover"
+        />
+      </picture>
+    </div>
+  </div>
+);
+
+export default Landing;
