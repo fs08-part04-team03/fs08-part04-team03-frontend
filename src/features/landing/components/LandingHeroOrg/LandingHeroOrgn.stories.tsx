a37aa368b1cf4c3ee19dbd7426cd2476d41bc499
Gibum <gudeng0611@cau.ac.kr>
2025-12-22 18:41:15 +0900

    fix: correct landingHeroOrgn file nme
     - coderabbit이 제안한 수정 사항 반영
       - landingHeorOrgn으로 잘못 작성되어있던 파일명을 제대로 수정

--- src/features/landing/components/LandingHeroOrg/LandingHeroOrgn.stories.tsx
+++ src/features/landing/components/LandingHeroOrg/LandingHeroOrgn.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
-import LandingHero from './LandingHeorOrgn';
+import LandingHero from './LandingHeroOrgn';
 
 /** =====================
  * Meta
