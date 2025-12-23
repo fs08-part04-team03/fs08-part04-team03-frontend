db91eddfb530b143141defbf38ced8db717bf510
Gibum <gudeng0611@cau.ac.kr>
2025-12-23 15:44:49 +0900

    feat: complete landing page with marquee
     - 랜딩 페이지 작성 마무리
     - template 생성 및 로직 분리
       - UI만을 구성하는 template 생성
       - section에서 marquee에 포함될 내용을 props로 내려주도록 설정
     - 한 페이지 안에서 모든 내용을 확인할 수 있게 수정
       - 별도의 스크롤바가 생기지 않도록 강제

---
+++ src/features/landing/template/LandingTem.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import LandingHero from '../components/LandingHeroOrg/LandingHeroOrgn';
+import { LandingMarqueeOrgn, MarqueeItem } from '../components/LandingMarquee/LandingMarqueeOrgn';
+
+interface LandingTemProps {
+  marqueeItems: MarqueeItem[];
+}
+
+const LandingTem = ({ marqueeItems }: LandingTemProps) => (
+  <div className="relative flex flex-col items-center w-full h-screen overflow-hidden bg-white">
+    <LandingHero />
+    {/* 반응형 이미지 (mobile, tablet, desktop에 따라 다른 이미지 사용) */}
+    <div className="w-full flex justify-center px-4 pb-20">
+      <picture>
+        <source media="(min-width: 1024px)" srcSet="/images/landing-desktop.svg" />
+        <source media="(min-width: 768px)" srcSet="/images/landing-tablet.svg" />
+        <img
+          src="/images/landing-mobile.svg"
+          alt="Snack App Interface"
+          className="
+                w-full 
+                max-w-375 tablet:max-w-768 desktop:max-w-1240
+                h-auto object-cover"
+        />
+      </picture>
+    </div>
+    <div className="fixed bottom-0 w-full z-[var(--z-toast)]">
+      <LandingMarqueeOrgn items={marqueeItems} />
+    </div>
+  </div>
+);
+
+export default LandingTem;
