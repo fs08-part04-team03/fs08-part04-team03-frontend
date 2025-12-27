cbef1a3bd99df987d321955e0f95742aef9d9f01
Aria / Ju Hyun Na <najuhyun.aria@gmail.com>
2025-12-27 18:03:34 +0900

    fix: Remove problematic import from next-env.d.ts
    eslint 규칙(import/extensions)과 충돌하는 import 문을 제거했습니다.
    - .next/types/routes.d.ts import 제거
    - Next.js 자동 생성 파일이므로 수동 수정 불필요

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-// eslint-disable-next-line import/extensions, import/no-unresolved
-import './.next/types/routes';
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
