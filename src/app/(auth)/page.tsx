6b688169883ec4d24856a74b2a47eeb10efdcbf5
Gibum <gudeng0611@cau.ac.kr>
2026-01-06 17:36:35 +0900

    refactor: add metadata and robots.txt to enhance SEO
     - metadata title 및 설명 추가
     - robots.txt 추가

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import Landing from '@/features/landing/section/Landing';
+
+export const metadata: Metadata = {
+  title: 'SNACK - 기업 간식 통합 관리',
+  description: '구매부터 정산까지 한 번에 관리하는 스마트한 기업 간식 솔루션',
+};
 
 const Home = () => <Landing />;
 
